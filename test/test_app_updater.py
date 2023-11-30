4d8f00e9151ac45597b819e5a18a8461eb102598
Matias Bertani <matiasbertani@gmail.com>
2023-11-29 21:44:18 -0300

    fix test

--- test/test_app_updater.py
+++ test/test_app_updater.py
@@ -15,6 +15,17 @@ from src.data_protocols import UserMessages
 from src.github_repository import GithubRepository
 
 
+@pytest.fixture
+def mock_github_repository() -> GithubRepository:
+    with patch.object(GithubRepository, '__init__', return_value=None):
+        github_repository = GithubRepository(
+            'fake_api_token',
+            'fake_organization_name',
+            'fake_repository_name',
+        )
+    return github_repository
+
+
 @pytest.mark.parametrize(
     "user_input, expected", [
         ('1234567890', '1234567890'),
@@ -269,16 +280,23 @@ def test_operator_wants(
     assert result == expected_result
 
 
-def test_update_to_new_version():
+@patch.object(GithubRepository, 'change_source_code_version')
+@patch.object(GithubRepository, 'get_commit_sha_of_version_tag')
+def test_update_to_new_version(
+    patched_get_commit_sha_of_version_tag: MagicMock,
+    patched_change_source_code_version: MagicMock,
+    mock_github_repository: GithubRepository,
+):
     # prepare
-    fake_commit_of_last_release = 'fake_commit_of_last_release'
-    mock_github_repository = Mock()
-    mock_github_repository.commit_of_last_release = fake_commit_of_last_release
+    fake_version_tag = 'fake_version_tag'
+    fake_version_commit = 'fake_version_commit'
+    patched_get_commit_sha_of_version_tag.return_value = fake_version_commit
 
     app_updater = AppUpdater()
 
     # test
-    app_updater._update_to_new_version(mock_github_repository)
+    app_updater._update_to_new_version(mock_github_repository, fake_version_tag)
 
     # asserts
-    mock_github_repository.change_source_code_version.assert_called_once_with(fake_commit_of_last_release)
+    mock_github_repository.get_commit_sha_of_version_tag.assert_called_once_with(fake_version_tag)
+    mock_github_repository.change_source_code_version.assert_called_once_with(fake_version_commit)
