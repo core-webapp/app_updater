62e8ba0eedb04825eeb7fc98544742bbac52047f
Walter Bucolo <bucolowalter@gmail.com>
2023-12-01 16:07:00 -0300

    Getting version control info from core-be

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 from src.app_updater import AppUpdater
 
 if __name__ == '__main__':
+    if not os.getenv("CORE_BE_TOKEN"):
+        print("No se ha configurado el token de acceso a core-be")
     AppUpdater().update_source_code_automaticaly()
---
+++ src/core_service.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+class CoreService:
+    BASE_URL = "http://localhost:8001/"
+
+    def __init__(self, token=None):
+        self.token = token if token else os.getenv("CORE_BE_TOKEN")
+    
+    def get_version_control_info(self):
+        url = f"{self.BASE_URL}api/company/version-control/me/"
+        response = requests.get(url, headers={"Authorization": f"Token {self.token}"})
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"ERROR AL OBTENER CONFIGURACION - {response.status_code} - {response.reason()}")
