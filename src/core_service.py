af5000935dfdcc83af2aadf7d4fbb5ef57732331
Walter Bucolo <bucolowalter@gmail.com>
2023-12-12 15:16:16 -0300

    Fixing comments

--- src/core_service.py
+++ src/core_service.py
@@ -3,17 +3,22 @@ import requests
 
 
 class CoreService:
-    BASE_URL = "https://core-be-production.up.railway.app/"
+    BASE_URL = os.getenv("CORE_BE_URL")
 
     def __init__(self, token=None):
         self.token = token if token else os.getenv("CORE_BE_TOKEN")
 
     def get_version_control_info(self):
         url = f"{self.BASE_URL}api/company/version-control/me/"
-        response = requests.get(url, headers={"Authorization": f"Token {self.token}"})
+        try:
+            response = requests.get(
+                url, headers={"Authorization": f"Token {self.token}"}
+            )
+        except Exception as e:
+            raise Exception(f"There was an error calling to core-be - {e}")
+
         if response.status_code == 200:
             return response.json()
-        else:
-            raise Exception(
-                f"ERROR AL OBTENER CONFIGURACION - {response.status_code} - {response.reason()}"
-            )
+        raise Exception(
+            f"There was an error getting the config - {response.status_code} - {response.reason()}"
+        )
